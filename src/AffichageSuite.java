8215dcec09a1828171b4a8e853c6c60cdb0a03b6
Sheerin Banu <sheemuzz@outlook.com>
2024-10-16 11:43:07 +0200

    AffichageSuite_2

--- src/AffichageSuite.java
+++ src/AffichageSuite.java
@@ -2,7 +2,9 @@ public class AffichageSuite {
     public static void main(String[] args) {
 
         for (int i = 1; i <= 10; i++) {
-          System.out.println("i = " + i);
+
+            if (i%2==0){
+          System.out.println("les nombres pairs = " + i);}
         }
     }
 }
