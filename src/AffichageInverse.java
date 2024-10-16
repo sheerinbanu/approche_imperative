dd4831e1438e2fc08c8eda9e2b034ffc694c05b1
Sheerin Banu <sheemuzz@outlook.com>
2024-10-16 15:07:59 +0200

    AffichageInverse

--- src/AffichageInverse.java
+++ src/AffichageInverse.java
@@ -6,17 +6,17 @@ public class AffichageInverse {
 
         int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
         for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
+            System.out.print(array[i]+" ");
         }
 
         for (int j = array.length - 1; j >= 0; j--) {
-            System.out.println(array[j]);
+            System.out.println(array[j]+" ");
         }
 
         int newLength = array.length;
         int[] arrayCopy = Arrays.copyOf(array, newLength);
         for (int m = 0; m < arrayCopy.length; m++) {
-            System.out.println(arrayCopy[m]);
+            System.out.print(arrayCopy[m]+" ");
         }
     }
 
