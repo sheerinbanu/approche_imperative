5d78e8dbb07ff644bc9366d6892d16d17b346205
Sheerin Banu <sheemuzz@outlook.com>
2024-10-16 10:17:11 +0200

    AffichageIdentite

---
+++ src/AffichageIdentite.java
@@ -0,0 +1,10 @@
+public class AffichageIdentite {
+    public static void main(String[] args) {
+
+
+        for (int i = 1; i <= 10; i++) {
+
+            System.out.println("My name is Sheerin Banu ");
+        }
+    }
+}
