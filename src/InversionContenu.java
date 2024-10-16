4ea9b1019bffad0d6001375d804b97819958581e
Sheerin Banu <sheemuzz@outlook.com>
2024-10-16 15:27:41 +0200

    AffichageInverse

--- src/InversionContenu.java
+++ src/InversionContenu.java
@@ -2,15 +2,14 @@ import java.util.Arrays;
 import java.util.Collections;
 
 public class InversionContenu {
-    static void reverse(Integer myArray[]){
+    static void reverse(Integer[] myArray) {
         Collections.reverse(Arrays.asList(myArray));
-        System.out.println("Reversed array" +Arrays.asList(myArray) );
+        System.out.println("Reversed array" + Arrays.asList(myArray));
     }
+
     public static void main(String[] args) {
         Integer[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
         reverse(array);
-
-
 
 
     }
