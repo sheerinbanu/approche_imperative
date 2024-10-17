953def54f96ac6c91306ab0634347288bb0718f4
Sheerin Banu <sheemuzz@outlook.com>
2024-10-17 16:28:34 +0200

    FirstClass

---
+++ src/FirstClass.java
@@ -0,0 +1,16 @@
+public class FirstClass {
+    public static void main(String[] args) {
+
+        int[] array;
+
+        int[] dummy = {1, 7, 8};
+        checkArray(dummy);
+        System.out.println(checkArray(dummy));
+    }
+
+    public static boolean checkArray(int[] array) {
+
+        return array.length>=1 && array[0] == array[array.length-1];
+    }
+
+}
